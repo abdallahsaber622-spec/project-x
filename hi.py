9fa7df5fc04937cda7338be2fb3c890912d670a8
abdallah saber <https://github.com/abdallahsaber0>
2026-06-22 15:39:41 +0300

    my asker

--- hi.py
+++ hi.py
@@ -1,6 +1,7 @@
-print ("Hello, World!")
-
-if True:
-    print("This is a test.")
+print("Hello, World!")
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!")
+age = input(" ammmm How old are you? ")
+print(f"You are {age} years old your year is so good.")
 
 
