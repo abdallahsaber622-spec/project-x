1cb773d4d29d0710107a9b5ad5d2fc67cfd52bc7
abdallah saber <https://github.com/abdallahsaber0>
2026-06-25 12:28:39 +0300

    make calcolator and make project 1

--- hi.py
+++ hi.py
@@ -1,8 +1,10 @@
+#التحدث معه في البدايه 
 print("Hello, World!")
 name = input("What is your name? ")
 print(f"Nice to meet you, {name}!")
 age = input(" ammmm How old are you? ")
 if int(age) >= 10:
+    #السؤال الاول
     print(f"You are {age} years old your year is so good.")
     print ('okay i give you some question' \
     ' What is the capital of France?')
@@ -10,12 +12,16 @@ if int(age) >= 10:
         print('Correct!')
         x=input('okay i can give you 1 more question? ')
         if x == 'yes' or x == 'Yes':
+            #السؤال الثاني
             print("okay what is the best country from visetours in africa ")
             if input() == 'egypt' or  'Egypt':    
                 print ('Correct! you are so smart')
                 input('okay i can give you 1 more question? but this is for smarters only okay it' \
                 'is in math and it is hard okay? or bye? ')
-                if input() == 'Okay' or 'okay':
+
+
+                if input() == 'Okay'or'okay':
+                    # السؤال الثالث للاذكياء فقط
                     print('okay what is answer of this'
                     'x=15-9*21'
                     'u=51'
@@ -25,26 +31,28 @@ if int(age) >= 10:
                 
                     if input() == '40587':
                         print('Correct! you are so so so so smart')
-                        input("do you want alse come at 12:00pm to see more")
+                        input("do you want play a small game?? or bye [yes play,no bye]")
+                        if input()=="yes play"or "Yes play":
+                            # بدايه اللعبه
+                            print("welcome to game ")
+                            print ("this game if you answer this last questions you is smart so " \
+                            "we see it in some questions what do you want We ask about him" \
+                            "[math or Ask a geographer about capital cities, or an astronomy student,"
+                            " or ask them random questions.]")
+
+
+
                     else:
                         print('Incorrect. The answer is 40587.')                
                 else:
-                    print ("")
+                    print ("okay bye")
                     
                    
             else:
-                    print('okay bye')            
-
-
-
-
+                print('okay bye')           
         else:
             print('okay bye')
-
     else:
         print('Incorrect. The capital of France is Paris.')
-
-
-
 else:    print(f"You are {age} years old your year is so bad.")
 
---
+++ more.py
@@ -0,0 +1,24 @@
+#my caculator
+#الفريبلز
+num1=print ("entre num1")
+
+num2=print ("enter num2")
+
+العلامات =print ("enter [+ or - or * or / ]")
+#الاخطاء الاملاءيه
+if num2 or num1 or العلامات!=int or float:
+    TypeError
+
+#العلامات
+if العلامات=="+":
+    print(num1 +num2)
+if العلامات=="-":
+    print(num1-num2)
+if العلامات=="*":
+    print(num1*num2)
+if العلامات=="/":
+    print(num1/num2)
+
+
+
+
